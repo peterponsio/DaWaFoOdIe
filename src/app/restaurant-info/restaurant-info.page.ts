d246cacf8cb9878f437c8cc74c906e21937e3473
peterponsio <pedrodeveloperera@gmail.com>
2020-02-12 01:42:35 +0100

    Todo ok con subida de imagen solo falta que se muestre

--- src/app/restaurant-info/restaurant-info.page.ts
+++ src/app/restaurant-info/restaurant-info.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router,NavigationExtras, ActivatedRoute } from '@angular/router';
 import { Route } from '@angular/compiler/src/core';
-
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-restaurant-info',
   templateUrl: './restaurant-info.page.html',
@@ -12,19 +12,26 @@ export class RestaurantInfoPage implements OnInit {
   unCO:any;
   originalArray:any;
 
-  constructor(private ruta:Router,public activatedRoute: ActivatedRoute) { }
+  constructor(private _sanitizer: DomSanitizer,private ruta:Router,public activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
 
   this.activatedRoute.params.subscribe(params=>{
   this.unCO = JSON.parse(params['data']);
+  this._sanitizer.bypassSecurityTrustUrl(this.unCO.img);
 })
     
-      console.log(this.unCO);
+      //console.log(this.unCO);
 
   }
 
+ /* getBackground(image) {
+    
+    console.log(image);
 
+    return this._sanitizer.bypassSecurityTrustUrl(image);
+  
+}*/
 
   onClose(){
     this.ruta.navigateByUrl("home");
